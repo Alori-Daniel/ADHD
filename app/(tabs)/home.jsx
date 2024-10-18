7179b36861fdd7a2a8994375a2f7fea3a3a9583f
Alori-Daniel <aloridaniel123@gmail.com>
2024-10-18 11:08:16 +0100

    first commit

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>welcome to Home</Text>
+    </View>
+  );
+};
+
+export default Home;
---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Welcome Daniel</Text>
+      <Text>Hello Daniel</Text>
+    </View>
+  );
+};
+
+export default Profile;
